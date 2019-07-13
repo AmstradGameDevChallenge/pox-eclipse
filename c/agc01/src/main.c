772a9ee5ff497854af43cd4c360b42b66ea4ade8
Miguel Angel Carvajal <macarvajal@gmail.com>
2019-07-13 18:49:20 +0100

    Some changes to make combat more interesting. Basic game loop.

--- c/agc01/src/main.c
+++ c/agc01/src/main.c
@@ -19,6 +19,7 @@ typedef struct {
    i8 attack;
    i8 defense;
    const char* name;
+   bool defending;
 } character_stats;
 
 void print_splash(void);
@@ -27,47 +28,72 @@ cpct_keyID key_pressed(cpct_keyID valid_keys[], u8 valid_keys_len);
 void wait_key(void);
 void player_action(character_stats *player, character_stats *enemy);
 void enemy_action(character_stats *player, character_stats *enemy);
+void resolve_attack(character_stats *foe, character_stats *attacker);
 void control_max_min_energy(character_stats *character);
+i8 max(i8, i8);
+i8 min(i8, i8);
+bool died(character_stats *stats);
 
 
 void main(void) {
 
-    character_stats player = {.agility=60, .energy = 100, .attack = 30, .defense = 15, .name="Max"};
-    character_stats enemy = {.agility= 50, .energy = 90, .attack = 20, .defense = 10, .name="mutard"};  
+    bool game_running = false;
+    character_stats player = {.agility=60, .energy = 100, .attack = 30, .defense = 15, .name="Max", .defending=false};
+    character_stats enemy = {.agility= 50, .energy = 90, .attack = 20, .defense = 10, .name="mutard", .defending=false};  
 
+    while (true) {
+        // Let's start!
+        print_splash();  
 
-    // Let's start!
-    print_splash();  
+        wait_key();
+        game_running = true;
+        while (game_running) {
+            // CLS
+            putchar(12);
 
-    wait_key();
-        
-    while (1) {
-         // CLS
-         putchar(12);
+        // Print stats
+            printf("PLAYER [%d] (a%d) (d%d)\r\n", player.energy, player.attack,   player.defense);
+            printf("ENEMY  [%d] (a%d) (d%d)\r\n", enemy.energy, enemy.attack, enemy.defense);
+            player.defending = false;
+            enemy.defending = false;
 
-       // Print stats
-        printf("PLAYER [%d] (a%d) (d%d)\r\n", player.energy, player.attack,   player.defense);
-        printf("ENEMY  [%d] (a%d) (d%d)\r\n", enemy.energy, enemy.attack, enemy.defense);
-
-        if (player.agility > enemy.agility){
-            println("Your agility allows you act before");
-            player_action(&player, &enemy);
-            enemy_action(&player, &enemy);
+            if (player.agility > enemy.agility){
+                println("Your agility allows you act before");
+                player_action(&player, &enemy);
+                enemy_action(&player, &enemy);                
+            }
+            else {            
+                println("Enemy is faster than you!");
+                enemy_action(&player, &enemy);
+                player_action(&player, &enemy);
+            }
             
+            if ( died(&player) ) {
+                println("You are severely injuried.\r\n You fall unconscious and\r\nyour rival kills you!");
+                game_running = false;
+            }
+            
+            if ( died(&enemy) ) {
+                println("Your last blow makes your enemy\r\nwalk back some steps.\r\nThen falls dead on the floor.");                
+                game_running = false;
+            }
+            
+            wait_key();
         }
-        else {            
-            println("Enemy is faster than you!");
-            enemy_action(&player, &enemy);
-            player_action(&player, &enemy);
-        }
-        
+
+        println("Game ended, will you play again?");
         wait_key();
     }
+}
+
+bool died(character_stats *stats) {
+    return stats->energy <= 0;
 }
 
 void print_splash()
 {
     //COLS:  1234567890123456789012345678901234567890
+    putchar(12);
     println("POX ECLIPSE");
     println("===========");
     println("Welcome to the end of the world!");
@@ -128,14 +154,12 @@ void player_action(character_stats *player, character_stats *enemy)
     switch( key ) {
         case Key_A: {            
             printf("Player %s attacts %s...\r\n", player->name, enemy->name );        
-            enemy->energy -= player->attack;
-            control_max_min_energy(enemy);
+            resolve_attack(enemy, player);
             break;
         }
         case Key_D: {            
             printf("Player %s defends of %s...\r\n", player->name, enemy->name );
-            player->energy += player->defense;
-            control_max_min_energy(player);
+            player->defending = true;
             break;
         }
     }
@@ -143,16 +167,29 @@ void player_action(character_stats *player, character_stats *enemy)
 
 void enemy_action(character_stats *player, character_stats *enemy)
 {
-    // ENEMY DECIDE
+    // ENEMY DECIDES
     if (cpct_rand() < 64) {
-        printf("Enemy %s defends of %s...\r\n", enemy->name, player->name );
-        enemy->energy += enemy->defense;        
-        control_max_min_energy(enemy);
+        printf("Enemy %s defends of %s...\r\n", enemy->name, player->name );         
+        enemy->defending=true;
 
     } else {
         printf("Enemy %s attacks %s...\r\n", enemy->name, player->name );
-        player->energy -= enemy->attack;
+        resolve_attack(player, enemy); 
         control_max_min_energy(player);
+    }
+}
+
+void resolve_attack(character_stats *foe, character_stats *attacker) {
+    i8 attack_strength = cpct_rand() % attacker->attack;
+    i8 damage = foe->defending? max(0, attack_strength-foe->defense) : attack_strength;
+    
+    foe->energy -= damage;
+    control_max_min_energy(foe);
+    if (damage > 0){
+        printf("%s makes %d points of damage!\r\n", attacker->name, damage);
+    }
+    else {
+        printf("%s's attack failed!\r\n", attacker->name, damage);
     }
 }
 
@@ -176,4 +213,22 @@ void wait_key() {
     do {
         key = key_pressed(wait_keys, wait_keys_len);
     } while (key == INVALID_KEY);
+}
+
+i8 max(i8 a, i8 b) {
+    if (a > b) { 
+        return a; 
+    }
+    else {
+        return b;
+    }
+}
+
+i8 min(i8 a, i8 b) {
+        if (a <= b) { 
+        return a; 
+    }
+    else {
+        return b;
+    }
 }
