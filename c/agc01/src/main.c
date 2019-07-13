b1d22bb235eccf20a881ea53e783d82f3064ec15
Miguel Angel Carvajal <macarvajal@gmail.com>
2019-07-13 22:33:52 +0100

    Defined different game states: start menu, explore, combat

--- c/agc01/src/main.c
+++ c/agc01/src/main.c
@@ -11,6 +11,11 @@
 #include <stdbool.h>
 
 #define NEW_LINE "\r\n"
+#define START_MENU  0
+#define COMBAT      1
+#define EXPLORATION 2
+#define ENDED       3
+ 
 cpct_keyID INVALID_KEY = 255;
 
 typedef struct {
@@ -22,68 +27,127 @@ typedef struct {
    bool defending;
 } character_stats;
 
+u8 game_state;
+void start_menu_state(void);
+void combat_state(void);
+void exploration_state(void);
+void endgame_state(void);
+
+character_stats player; 
+character_stats enemy;
+
 void print_splash(void);
 void println(char *string);
 cpct_keyID key_pressed(cpct_keyID valid_keys[], u8 valid_keys_len);
 void wait_key(void);
 void player_action(character_stats *player, character_stats *enemy);
 void enemy_action(character_stats *player, character_stats *enemy);
+void player_explore_action(character_stats *player);
 void resolve_attack(character_stats *foe, character_stats *attacker);
 void control_max_min_energy(character_stats *character);
 i8 max(i8, i8);
 i8 min(i8, i8);
 bool died(character_stats *stats);
 
+void create_player() {
+    player.agility=60;
+    player.energy = 100;
+    player.attack = 30;
+    player.defense = 15;
+    player.name="Max";
+    player.defending=false;
+}
 
-void main(void) {
+void create_enemy() {
+    enemy.agility= 50;
+    enemy.energy = 90;
+    enemy.attack = 20;
+    enemy.defense = 10;
+    enemy.name="a mutard";
+    enemy.defending=false;  
+}
 
-    bool game_running = false;
-    character_stats player = {.agility=60, .energy = 100, .attack = 30, .defense = 15, .name="Max", .defending=false};
-    character_stats enemy = {.agility= 50, .energy = 90, .attack = 20, .defense = 10, .name="mutard", .defending=false};  
-
+void main(void) {        
+    game_state = 0;
     while (true) {
-        // Let's start!
-        print_splash();  
-
-        wait_key();
-        game_running = true;
-        while (game_running) {
-            // CLS
-            putchar(12);
-
-        // Print stats
-            printf("PLAYER [%d] (a%d) (d%d)\r\n", player.energy, player.attack,   player.defense);
-            printf("ENEMY  [%d] (a%d) (d%d)\r\n", enemy.energy, enemy.attack, enemy.defense);
-            player.defending = false;
-            enemy.defending = false;
-
-            if (player.agility > enemy.agility){
-                println("Your agility allows you act before");
-                player_action(&player, &enemy);
-                enemy_action(&player, &enemy);                
+        switch( game_state ) {
+            case START_MENU: {
+                create_player();
+                start_menu_state();
+                break;
             }
-            else {            
-                println("Enemy is faster than you!");
-                enemy_action(&player, &enemy);
-                player_action(&player, &enemy);
+            case COMBAT: {                
+                combat_state();
+                break;
             }
-            
-            if ( died(&player) ) {
-                println("You are severely injuried.\r\n You fall unconscious and\r\nyour rival kills you!");
-                game_running = false;
+            case EXPLORATION: {
+                exploration_state();
+                break;
             }
-            
-            if ( died(&enemy) ) {
-                println("Your last blow makes your enemy\r\nwalk back some steps.\r\nThen falls dead on the floor.");                
-                game_running = false;
+            case ENDED: {
+                endgame_state();
+                break;
             }
-            
-            wait_key();
         }
-
-        println("Game ended, will you play again?");
         wait_key();
     }
+}
+
+
+void start_menu_state(){
+    // Let's start!
+    print_splash();  
+    game_state = EXPLORATION;
+}
+
+void combat_state() {
+    bool fighting = true;
+
+    // CLS
+    putchar(12);
+
+    // Print stats
+    printf("PLAYER [%d] (a%d) (d%d)\r\n", player.energy, player.attack,   player.defense);
+    printf("ENEMY  [%d] (a%d) (d%d)\r\n\n\n", enemy.energy, enemy.attack, enemy.defense);
+    printf("a - Attack. d - Defend\r\n");
+    player.defending = false;
+    enemy.defending = false;
+
+    if (player.agility > enemy.agility){
+        println("Your agility allows you act before");
+        player_action(&player, &enemy);
+        enemy_action(&player, &enemy);                
+    }
+    else {            
+        println("Enemy is faster than you!");
+        enemy_action(&player, &enemy);
+        player_action(&player, &enemy);
+    }
+    
+    if ( died(&player) ) {
+        println("You are severely injuried.\r\n You fall unconscious and\r\nyour rival kills you!");
+        fighting = false;
+        game_state = ENDED;
+    }
+    
+    if ( died(&enemy) ) {
+        println("Your last blow makes your enemy\r\nwalk back some steps.\r\nThen falls dead on the floor.");                
+        fighting = false;
+        game_state = EXPLORATION;
+    }        
+}
+
+void exploration_state() {
+    // Print stats
+    putchar(12);
+    printf("PLAYER [%d] (a%d) (d%d)\r\n", player.energy, player.attack,   player.defense);
+    printf("m - Move. r - Rest. \r\n");
+    player_explore_action(&player);
+}
+
+void endgame_state() {
+    println("Game ended, will you play again?");
+    game_state = START_MENU;
 }
 
 bool died(character_stats *stats) {
@@ -98,7 +162,7 @@ void print_splash()
     println("===========");
     println("Welcome to the end of the world!");
     println("Nobody knows how many years have passed");
-    println("from the Big Boum Boum, now the earth");
+    println("from the Big Boum-Boum. now the earth");
     println("is poisoned and everyone's life is");
     println("blood and fire. Day after day, you have");
     println("managed to survive in this barren land,");
@@ -141,6 +205,34 @@ cpct_keyID key_pressed(cpct_keyID valid_keys[], u8 valid_keys_len)
    return(key_caption);
 }
 
+void player_explore_action(character_stats *player)
+{
+    cpct_keyID command_keys[] = {Key_M, Key_R};
+    u8 command_keys_len = 2;
+    u8 energy_gained = 0;    
+    cpct_keyID key;
+
+    key = key_pressed(command_keys, command_keys_len);
+    switch(key) {
+        case(Key_R): {
+            println("You rest for some time...");
+            energy_gained = max(0, min(cpct_rand(), 10));
+            player->energy += energy_gained;
+            control_max_min_energy(player);
+            break;
+        }
+        case (Key_M): {
+            println("You explore the zone...");
+            if (cpct_rand()>100) {
+                create_enemy();
+                printf("And find %s!!!", enemy.name);
+                
+                game_state = COMBAT;
+            }
+            break;
+        }
+    }
+}
 
 void player_action(character_stats *player, character_stats *enemy)
 {
@@ -169,13 +261,12 @@ void enemy_action(character_stats *player, character_stats *enemy)
 {
     // ENEMY DECIDES
     if (cpct_rand() < 64) {
-        printf("Enemy %s defends of %s...\r\n", enemy->name, player->name );         
+        printf("%s defends of %s...\r\n", enemy->name, player->name );         
         enemy->defending=true;
 
     } else {
-        printf("Enemy %s attacks %s...\r\n", enemy->name, player->name );
-        resolve_attack(player, enemy); 
-        control_max_min_energy(player);
+        printf("%s attacks %s...\r\n", enemy->name, player->name );
+        resolve_attack(player, enemy);
     }
 }
 
@@ -194,14 +285,14 @@ void resolve_attack(character_stats *foe, character_stats *attacker) {
 }
 
 void control_max_min_energy(character_stats *character) {
-        if (character->energy > 100) {
-            character->energy = 100;
+    if (character->energy > 100) {
+        character->energy = 100;
+    }
+    else {
+        if (character->energy <= 0) {
+            character->energy = 0;
         }
-        else {
-            if (character->energy <= 0) {
-                character->energy = 0;
-            }
-        }
+    }
 }
 
 void wait_key() {
