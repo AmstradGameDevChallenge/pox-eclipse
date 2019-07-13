8cfb901007a98e6bbf8c99657501a003f1000da4
Miguel Angel Carvajal <macarvajal@gmail.com>
2019-07-13 10:50:42 +0100

    Using switch to select player actions

--- c/agc01/src/main.c
+++ c/agc01/src/main.c
@@ -8,66 +8,61 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define NEW_LINE "\r\n"
+cpct_keyID INVALID_KEY = 255;
 
 typedef struct {
+   u8 agility;
    u8 energy;
    u8 attack;
    u8 defense;
    const char* name;
 } character_stats;
 
-void print_splash();
+void print_splash(void);
 void println(char *string);
+cpct_keyID key_pressed(cpct_keyID valid_keys[], u8 valid_keys_len);
+void wait_key(void);
+void player_action(character_stats *player, character_stats *enemy);
+void enemy_action(character_stats *player, character_stats *enemy);
+void control_max_min_energy(character_stats *character);
+
 
 void main(void) {
 
-    character_stats player = {.energy = 100, .attack = 30, .defense = 15, .name="Max"};
-    character_stats enemy = {.energy = 90, .attack = 20, .defense = 10, .name="mutard"};  
-   
-   // Let's start!
-   print_splash();   
-   
-   // Is enter pressed?
-   do {
-        cpct_scanKeyboard();        
-   }
-   while (!cpct_isKeyPressed(Key_Enter));
+    character_stats player = {.agility=60, .energy = 100, .attack = 30, .defense = 15, .name="Max"};
+    character_stats enemy = {.agility= 50, .energy = 90, .attack = 20, .defense = 10, .name="mutard"};  
 
-   while (1) {
-           
-        // CLS
-        putchar(12);
 
-      // Print stats
-      printf("PLAYER [%d] (a%d) (d%d)\r\n", player.energy,   player.attack,   player.defense);
-      printf("ENEMY  [%d] (a%d) (d%d)\r\n", enemy.energy, enemy.attack, enemy.defense);
-      
-      // Re scan keyboard
-      cpct_scanKeyboard();
-      
-      while (!cpct_isKeyPressed(Key_A) && !cpct_isKeyPressed(Key_D)) {
-         cpct_scanKeyboard();
-      }
-         
-      // PLAYER ATTACKS!
-      if (cpct_isKeyPressed(Key_A)) {
-         enemy.energy -= player.attack;
-      } else {
-         // PLAYER DEFENDS!
-         if (cpct_isKeyPressed(Key_D)) {
-            player.energy += player.defense;
-         }
-      }
-      
-      // ENEMY DECIDE
-      if (cpct_rand() < 64) {
-         enemy.energy += enemy.defense;
-      } else {
-         player.energy -= player.attack;
-      }
-   }
+    // Let's start!
+    print_splash();  
+
+    wait_key();
+        
+    while (1) {
+         // CLS
+         putchar(12);
+
+       // Print stats
+        printf("PLAYER [%d] (a%d) (d%d)\r\n", player.energy, player.attack,   player.defense);
+        printf("ENEMY  [%d] (a%d) (d%d)\r\n", enemy.energy, enemy.attack, enemy.defense);
+
+        if (player.agility > enemy.agility){
+            println("Your agility allows you act before");
+            player_action(&player, &enemy);
+            enemy_action(&player, &enemy);
+            
+        }
+        else {            
+            println("Enemy is faster than you!");
+            enemy_action(&player, &enemy);
+            player_action(&player, &enemy);
+        }
+        
+        wait_key();
+    }
 }
 
 void print_splash()
@@ -88,11 +83,97 @@ void print_splash()
     println("your soul, a tiny spark of hope");
     println("brights");
     println("");
-    println("PRESS ENTER TO START");
+    
 }
 
 void println(char *string)
 {
     printf(string);
     printf(NEW_LINE);
+}
+
+cpct_keyID key_pressed(cpct_keyID valid_keys[], u8 valid_keys_len) 
+{
+   bool valid_key_pressed = false;
+   cpct_keyID key_caption = 255;
+
+   do 
+   {
+        cpct_scanKeyboard(); 
+
+        for (u8 i=0; i< valid_keys_len; ++i)  
+        {            
+            cpct_keyID current_key = valid_keys[i];
+            
+            if (cpct_isKeyPressed(current_key)) {
+                valid_key_pressed = true;
+                key_caption = current_key;
+            }
+        }    
+   } while(!valid_key_pressed);
+
+   return(key_caption);
+}
+
+
+void player_action(character_stats *player, character_stats *enemy)
+{
+    cpct_keyID command_keys[] = {Key_A, Key_D};
+    u8 command_keys_len = 2;
+    cpct_keyID key;
+
+    println("Press key for next move...");
+    
+    key = key_pressed(command_keys, command_keys_len);
+    switch( key ) {
+        case Key_A: {            
+            printf("Player %s attacts %s...\r\n", player->name, enemy->name );        
+            enemy->energy -= player->attack;
+            control_max_min_energy(enemy);
+            break;
+        }
+        case Key_D: {            
+            printf("Player %s defends of %s...\r\n", player->name, enemy->name );
+            player->energy += player->defense;
+            control_max_min_energy(player);
+            break;
+        }
+    }
+}
+
+void enemy_action(character_stats *player, character_stats *enemy)
+{
+    // ENEMY DECIDE
+    if (cpct_rand() < 64) {
+        printf("Enemy %s defends of %s...\r\n", enemy->name, player->name );
+        enemy->energy += enemy->defense;        
+        control_max_min_energy(enemy);
+
+    } else {
+        printf("Enemy %s attacks %s...\r\n", enemy->name, player->name );
+        player->energy -= enemy->attack;
+        control_max_min_energy(player);
+    }
+}
+
+void control_max_min_energy(character_stats *character) {
+        if (character->energy > 100) {
+            character->energy = 100;
+        }
+        else {
+            if (character->energy <= 0) {
+                character->energy = 0;
+            }
+        }
+}
+
+void wait_key() {
+    cpct_keyID wait_keys[] = {Key_Enter};
+    u8 wait_keys_len = 1;
+    cpct_keyID key; 
+    
+    println("PRESS ENTER TO CONTINUE");
+    do {
+        key = key_pressed(wait_keys, wait_keys_len);
+    } while (key == INVALID_KEY);
 }
